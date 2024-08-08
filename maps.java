2e22352408067bb8ec36b5ffc774a28789142f80
NipunRajput <nipun.rajput6586s@gmail.com>
2024-08-09 01:23:12 +0530

    Commit

---
+++ maps.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class maps{
+    public static void main(String[]args){
+
+        HashMap<String,Integer> hmap=new HashMap<>();
+
+
+        hmap.put("Yolo",8899);
+        System.out.println(hmap);
+    }
+}
